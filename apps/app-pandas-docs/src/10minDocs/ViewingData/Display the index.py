1ff413cb32f0a0c0193cd734a4fed46175281ad8
aozdemir <aozdemir@webmd.net>
2018-10-17 14:11:34 -0400

    pandas pundas

--- apps/app-pandas-docs/src/10minDocs/ViewingData/Display the index.py
+++ apps/app-pandas-docs/src/10minDocs/ViewingData/Display the index.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 dates = pd.date_range('20130101', periods=6)
 df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
